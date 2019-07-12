41fce92a5873172f235b20fab6dc08316bc748ba
Nayna <nainavaghasiya9@gmail.com>
2019-07-11 22:18:41 -0400

    Internet.swift file added

--- C0753560_MidTerm_MAD3115S2019/C0753560_MidTerm_MAD3115S2019/SwiftClasses/Hydro.swift
+++ C0753560_MidTerm_MAD3115S2019/C0753560_MidTerm_MAD3115S2019/SwiftClasses/Hydro.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+class Hydro : Bill
+{
+    var agencyName : String
+    var unitConsumed : Float
+    let unitRate : Float = 10
+    
+    override init()
+    {
+        self.agencyName = ""
+        self.unitConsumed = 0
+        super.init()
+    }
+    init(billId: Int, billDate: Date, billType: BillType,agencyName : String,unitConsumed : Float )
+    {
+        self.agencyName = agencyName
+        self.unitConsumed = unitConsumed
+        super.init(billId: billId, billDate: billDate, billType: billType,totalBillAmount: 0)
+    }
+    
+    override func calculateTotal()->Float
+    {
+        var total : Float = 0
+        total = self.unitConsumed * self.unitRate
+        self.totalBillAmount = total
+        return total
+    }
+    
+    override func display()
+    {
+        print("Bill id : ",self.billId)
+        print("Bill Date : ",self.billDate.getForamttedDate())
+        print("Bill Type : ",self.billType)
+        print("Bill Amount : ",self.totalBillAmount.currency())
+        print("Agency Name : ",self.agencyName)
+        print("Unit Consumed : ",self.unitConsumed.formatUnits())
+}
+}
--- C0753560_MidTerm_MAD3115S2019/C0753560_MidTerm_MAD3115S2019/SwiftClasses/Internet.swift
+++ C0753560_MidTerm_MAD3115S2019/C0753560_MidTerm_MAD3115S2019/SwiftClasses/Internet.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+class Internet : Bill
+{
+    var providerName : String
+    var internetGBused : Float
+    let internetRate : Float = 10
+    
+    override init()
+    {
+        self.providerName = ""
+        self.internetGBused = 0
+        super.init()
+    }
+    
+    init(billId: Int, billDate: Date, billType: BillType,providerName : String,internetGBused : Float)
+    {
+        self.providerName = providerName
+        self.internetGBused = internetGBused
+        super.init(billId: billId, billDate: billDate, billType: billType, totalBillAmount: 0)
+    }
+    
+    override func calculateTotal()->Float
+    {
+        var total : Float = 0
+        total = self.internetGBused * self.internetRate
+        self.totalBillAmount = total
+        return total
+    }
+    
+    override func display()
+    {
+        print("Bill id : ",self.billId)
+        print("Bill Date : ",self.billDate.getForamttedDate())
+        print("Bill Type : ",self.billType)
+        print("Bill Amount : ",self.totalBillAmount.currency())
+        print("Provider Name : ",self.providerName)
+        print("Internet Usage : ",self.internetGBused.formatGb())
+}
+}
