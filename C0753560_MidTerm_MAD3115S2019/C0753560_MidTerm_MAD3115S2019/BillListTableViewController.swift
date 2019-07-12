ed3f6e46b50ee2876efc7a65dd55e57a0f0dea15
Nayna <nainavaghasiya9@gmail.com>
2019-07-12 12:55:06 -0400

    BillListViewController added and layout set done

---
+++ C0753560_MidTerm_MAD3115S2019/C0753560_MidTerm_MAD3115S2019/BillListTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BillListTableViewController.swift
+//  C0753560_MidTerm_MAD3115S2019
+//
+//  Created by Nayna on 7/12/19.
+//  Copyright © 2019 Nayna. All rights reserved.
+//
+
+import UIKit
+
+class BillListTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
